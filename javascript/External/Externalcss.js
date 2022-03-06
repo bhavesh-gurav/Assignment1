95544b811f70164147c483848777fcb7d246da74
bhavesh <bhavesh.gurav.in@gmail.com>
2022-03-06 23:35:48 +0530

    first commit

---
+++ javascript/External/Externalcss.js
@@ -0,0 +1,9 @@
+function Externalalert() {
+    alert("This is Internal Event of alert")
+}
+function Externalprompt() {
+    alert("This is Internal Event of Prompt")
+}
+function Externalconfirm() {
+    alert("This is Internal Event of confirm")
+}
